4613f144ed372a6884587e0d1380d8edc7783dd5
ysoktsk <hnkc.ysoktsk@gmail.com>
2023-11-03 14:02:05 +0900

    deeper into go

---
+++ a-simple-start/main.go
@@ -0,0 +1,21 @@
+// What does 'package main' mean?
+// package == project == workspace
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
+
+// <How do we run the code in our project?>
+// go run main.go
+
+// <go cli>
+// go build: compiles a bunch of go source code files
+// go run: compiles and executes one or two files 
+// go fmt: Format all the code in each file in the current directory
+// go install: compiles and 'installs' a package
+// go get: Downaloads the raw source cdoe of someone else's package
+
+
---
+++ deeper-into-go/cards/deck.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string 
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
+// receicer
+// any variable of type "deck" now gets access to the "print" method
+func (d deck) print() {
+		for i, card := range d {
+		fmt.Println(card, i)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:] 
+}
